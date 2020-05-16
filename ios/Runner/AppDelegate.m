9dc10c513376a8b0475412f85abebef0ed63379b
SergioVarelaB <sergioalberto.varelaB@gmail.com>
2020-05-16 09:01:38 -0600

    Routes and map file

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+@import GoogleMaps;
 
 @implementation AppDelegate
 
@@ -7,6 +8,7 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [GMSServices provideAPIKey:@"AIzaSyBsNUuK7CDu-Vrd2bwJX0GsaDxP429qk28"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
