ce8d5827873b2ccd60d2bf30d9345fc6773600b8
Luisfayre <luisfayre@hotmail.com>
2020-05-29 23:10:56 -0600

    Se crea archivo de rutas

---
+++ lib/routes/routes.dart
@@ -0,0 +1,17 @@
+import 'package:apphibridatrabajos/src/login_page.dart';
+import 'package:flutter/material.dart';
+
+import '../detail_page.dart';
+import '../main.dart';
+
+
+
+Map<String, WidgetBuilder> getAplicationRoutes(){
+
+  return <String, WidgetBuilder>{
+    '/': (BuildContext context) => LoginPage(),
+    'login': (BuildContext context) => HomeWidget(),
+    'principal': (BuildContext context) => DetailPage(),
+  };
+}
+
